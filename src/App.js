cd9acd9ed701d05093ce175c8cce2243d7f73dd7
Simo Koulu <simo.koulu@gmail.com>
2021-11-30 11:49:13 +0200

    valmis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // https://www.sitepoint.com/react-tutorial-build-calculator-app/
 
-import Wrapper from './components/Wrapper';
-import Screen from './components/Screen';
-import ButtonBox from './components/ButtonBox';
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import React, { useState } from "react";
 
-const btValues = [
+const btnValues = [
   ["C", "+-", "%", "/"],
   [7, 8, 9, "X"],
   [4, 5, 6, "-"],
@@ -13,34 +14,152 @@ const btValues = [
   [0, ".", "="],
 ];
 
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
 
-function App() {
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+
+const App = () => {
+
+  const [pressed, setPressed] = useState("");
+  
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : (sign = "-" ? a - b : sign === "X" ? a * b : a / b);
+
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Et voi jakaa nollalla"
+            : toLocaleString(
+                math(
+                  Number(removeSpaces(calc.res)),
+                  Number(removeSpaces(calc.num)),
+                  calc.sign
+                )
+              ),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+
+  const invertClickHandler = () => {
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
+      sign: "",
+    });
+  };
+
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
   return (
     <div className="app">
-
-      <Wrapper> 
-        <Screen value="0" />
+      <h1>{pressed}</h1>
+      <Wrapper>
+        <Screen value={calc.num ? calc.num : calc.res} />
         <ButtonBox>
-          {
-            btValues.flat().map((btn, i) => {
-              return (
-                  <Button
-                  className={btn === "=" ? "equals" : ""}
-                  value={btn}
-                  onClick={() => {
-                    console.log(`Painoit : ${btn}`);
-                  }}
-                />
-              );
-            })
-          }         
+          {btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={
+                  // console.log(`Painoit : ${btn}`);
+                  // setPressed(btn);
+                  btn === "C"
+                    ? resetClickHandler
+                    : btn === "+-"
+                    ? invertClickHandler
+                    : btn === "%"
+                    ? percentClickHandler
+                    : btn === "="
+                    ? equalsClickHandler
+                    : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                    ? signClickHandler
+                    : btn === "."
+                    ? commaClickHandler
+                    : numClickHandler
+                }
+              />
+            );
+          })}
         </ButtonBox>
-
       </Wrapper>
-
-
     </div>
   );
-}
+};
 
 export default App;
